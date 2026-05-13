c568fa973edd4934b7fd6f4352a5c25b94457b9f
jiantaoyuNWAFU <jiantao.yu@nwafu.edu.cn>
2026-05-13 16:41:35 +0800

    Update run_virus-human.sh

--- scripts/run_virus-human.sh
+++ scripts/run_virus-human.sh
@@ -14,18 +14,20 @@ mkdir -p "$SAVE_DIR"
 mkdir -p "$LOG_DIR"
 
 nohup env CUDA_VISIBLE_DEVICES=0 \
-python -u ./run/train.py \
+python -u ./run/train_fuse.py \
 --cuda \
 --interaction_data "$INTERACTION_DATA" \
 --sequence_data "$SEQUENCE_DATA" \
 --save_dir "$SAVE_DIR" \
 --batch_size 128 \
 --g_steps 1 \
---beta_fake_loss 0.005 \
---lambda_align 0.1 \
---lambda_freq 10.0 \
---noise_scale 0.1 \
+--beta_fake_loss 0.05 \
+--lambda_freq 20.0 \
+--freq_warmup_epochs 0 \
+--noise_scale 0.5 \
 --seed 42 \
+--lambda_entropy 0.01 \
+--max_save_fake 128 \
 > "$LOG_DIR/train.log" 2>&1 &
 
 echo "Started training"
